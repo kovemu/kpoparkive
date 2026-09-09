bf9ef4214560955cfa9a7093fe9e017a68272a38
kovemu <kovemusin@gmail.com>
2026-09-09 22:02:05 +0900

    feat: wire raw asset resolver into extension

--- tools/namu-image-capture-extension/background-controller.js
+++ tools/namu-image-capture-extension/background-controller.js
@@ -1,1 +1,1 @@
-importScripts("background.js", "helper-clone-controller.js", "fidelity-controller.js");
+importScripts("background.js", "raw-asset-resolver.js", "helper-clone-controller.js", "fidelity-controller.js");
