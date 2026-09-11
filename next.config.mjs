64e7e33abe3d13fd13e522ddac3dd5f5659bddf3
kovemu <kovemusin@gmail.com>
2026-09-11 12:48:17 +0900

    fix: keep The Tree preview worker packages external in Next runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: [
+    "piscina",
+    "thetree",
+    "isolated-vm"
+  ],
   outputFileTracingIncludes: {
     "/api/wiki-source-preview": [
-      "./node_modules/thetree/utils/namumark/**/*",
-      "./node_modules/thetree/utils/global.js",
-      "./node_modules/thetree/utils/types.js",
-      "./node_modules/thetree/utils/index.js"
+      "./node_modules/piscina/**/*",
+      "./node_modules/thetree/**/*",
+      "./node_modules/isolated-vm/**/*"
     ]
   }
 };
