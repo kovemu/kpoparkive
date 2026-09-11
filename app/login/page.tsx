652b122285414d06a2edd5e2ec7260e90e27a4f7
kovemu <kovemusin@gmail.com>
2026-09-11 14:10:44 +0900

    feat: simplify mobile-first wiki navigation

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import SiteHeader from "../../components/wiki/SiteHeader";
+
+export default function LoginPage() {
+  return (
+    <>
+      <SiteHeader />
+      <main className="simplePage">
+        <h1>Log in</h1>
+        <p>Account login is coming soon.</p>
+      </main>
+    </>
+  );
+}
